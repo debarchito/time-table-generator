99bb57bb601279ba63e109711666d6d8553ebd7a
Debarchito Nath <debarchiton@proton.me>
2025-09-11 11:06:40 +0530

    feat(scaffold): base scaffold

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "time-table-generator";
+            venvDir = ".venv";
+            packages = [
+              pkgs.python313
+              pkgs.python313Packages.venvShellHook
+              pkgs.uv
+            ];
+          };
+        };
+    };
+}
